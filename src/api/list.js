d87aa0f10ca618e0828bc6c053039562bf175c7e
Gia Nguyên Ngô <nguyen.ng@teko.vn>
2020-06-23 10:21:44 +0700

    Add add to favorite list

--- src/api/list.js
+++ src/api/list.js
@@ -94,4 +94,95 @@ router.get('/list/favorite', validateUser, async (req, res) => {
     return buildRes(res, false, e.toString());
   }
 });
+
+/**
+ * @swagger
+ *
+ * /list:
+ *  get:
+ *    security:
+ *      - Bearer: []
+ *    summary: Get list array  by user
+ *    description: Get the list user have
+ *    tags:
+ *      - list
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      '200':
+ *        description: OK
+ *        schema:
+ *          type: object
+ *          properties:
+ *            success:
+ *              type: boolean
+ *            data:
+ *              type: array
+ *              items:
+ *                  $ref: '#/definitions/List'
+ */
+
+router.get('/list', validateUser, async (req, res) => {
+  try {
+    const token = getTokenByRequest(req);
+    const userId = await getUserIdByToken(token);
+    const filmList = await ListService.getListByUser(userId);
+    return buildRes(res, true, filmList);
+  } catch (e) {
+    return buildRes(res, false, e.toString());
+  }
+});
+
+/**
+ * @swagger
+ *
+ * /list/favorite/add:
+ *  post:
+ *    security:
+ *      - Bearer: []
+ *    summary: Add to film to favorite list of user
+ *    description: Add film to favorite list of the owner of the token
+ *    tags:
+ *      - list
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - in: body
+ *        name: body
+ *        required: true
+ *        schema:
+ *          type: object
+ *          required:
+ *            - filmId
+ *          properties:
+ *            filmId:
+ *              type: string
+ *    responses:
+ *      '200':
+ *        description: OK
+ *        schema:
+ *          type: object
+ *          properties:
+ *            success:
+ *              type: boolean
+ *            data:
+ *              type: object
+ *              properties:
+ *                listId:
+ *                  type: integer
+ *                filmId:
+ *                  type: integer
+ */
+
+router.post('/list/favorite/add', validateUser, async (req, res) => {
+  try {
+    const token = getTokenByRequest(req);
+    const userId = await getUserIdByToken(token);
+    const { filmId } = req.body;
+    const filmList = await ListService.addToFavoriteList(filmId, userId);
+    return buildRes(res, true, filmList);
+  } catch (e) {
+    return buildRes(res, false, e.toString());
+  }
+});
 module.exports = router;
--- src/services/list_service.js
+++ src/services/list_service.js
@@ -39,6 +39,55 @@ const ListService = {
       throw new ResponseException(e.toString());
     }
   },
+
+  async createFavoriteList(userId) {
+    try {
+      const userFavoriteList = await db.Lists.findOne({
+        where: {
+          userId,
+          favorite: true,
+        },
+      });
+      if (userFavoriteList) {
+        throw new ResponseException('Favorite list has exist');
+      } else {
+        const createdFavoriteList = await db.Lists.create({
+          listName: 'Favorite',
+          favorite: true,
+          userId,
+        });
+        return createdFavoriteList;
+      }
+    } catch (e) {
+      throw new ResponseException(e.toString());
+    }
+  },
+
+  async addToFavoriteList(filmId, userId) {
+    try {
+      let userFavoriteList = await db.Lists.findOne({
+        where: {
+          userId,
+          favorite: true,
+        },
+      });
+      if (!userFavoriteList) {
+        userFavoriteList = await this.createdFavoriteList(userId);
+      }
+      const [filmList, isCreate] = await db.FilmsLists.findOrCreate({
+        where: {
+          filmId,
+          listId: userFavoriteList.listId,
+        },
+      });
+      if (isCreate) {
+        return filmList;
+      }
+      throw new ResponseException('This film has add to list');
+    } catch (e) {
+      throw new ResponseException(e.toString());
+    }
+  },
 };
 
 module.exports = ListService;
